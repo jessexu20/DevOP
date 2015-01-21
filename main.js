759522d5c1d803a5a19fbad29cffce34f68e63a0
Jesse Xu <jessexu@Jesses-MacBook-Air.local>
2015-01-21 11:47:21 -0500

    add screenshots

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ AWS.config.update({region: 'us-east-1'});
 
 var ec2 = new AWS.EC2();
 
-//##List Available Zone
+// ##List Available Zone
 // var params = {
 //   DryRun:  false,
 // };
@@ -23,14 +23,14 @@ var params = {
     /* more items */
   ],
 };
-// ec2.startInstances(params, function(err, data) {
-//   if (err) console.log(err, err.stack); // an error occurred
-//   else{// successful response
-// 	  for(var i=0;i<data.StartingInstances.length;i++){
-// 	  	console.log(data.StartingInstances[i]);
-// 	  }
-//   }
-// });
+ec2.startInstances(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else{// successful response
+	  for(var i=0;i<data.StartingInstances.length;i++){
+	  	console.log(data.StartingInstances[i]);
+	  }
+  }
+});
 
 //####stop instance
 // ec2.stopInstances(params, function(err, data) {
@@ -43,28 +43,31 @@ var params = {
 // });
 
 //#### describe the instance
-ec2.describeInstances(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else{
-	  // console.log(data);           // successful response
-	  for(var i=0;i<data.Reservations.length;i++){
-	  	console.log("ReservationId="+data.Reservations[i].ReservationId);
-		console.log("OwnerId="+data.Reservations[i].OwnerId);
-		console.log("Groups="+data.Reservations[i].Groups);
-		for(var j=0;j<data.Reservations[i].Instances.length;j++){
-			console.log(data.Reservations[i].Instances[j]);
-		}
-	  }
-	  	
-  }
-});
+// ec2.describeInstances(params, function(err, data) {
+//   if (err) console.log(err, err.stack); // an error occurred
+//   else{
+// 	  // console.log(data);           // successful response
+// 	  for(var i=0;i<data.Reservations.length;i++){
+// 	  	console.log("ReservationId="+data.Reservations[i].ReservationId);
+// 		console.log("OwnerId="+data.Reservations[i].OwnerId);
+// 		console.log("Groups="+data.Reservations[i].Groups);
+// 		for(var j=0;j<data.Reservations[i].Instances.length;j++){
+// 			console.log(data.Reservations[i].Instances[j]);
+// 		}
+// 	  }
+//
+//   }
+// });
 
-//#Reboot Instances
-
+// #Reboot Instances
+//
 // ec2.rebootInstances(params, function(err, data) {
 //   if (err) console.log(err, err.stack); // an error occurred
-//   else
+//   else{
+// 	  console.log("Rebooting");
 // 	  console.log(data);           // successful response
+//   }
+//
 // });
 
 
